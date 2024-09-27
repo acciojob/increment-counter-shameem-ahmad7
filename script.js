f8117e23ca84da36c2ec2a0dd0114bfe6c1b5601
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-27 14:32:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const counterElement = document.getElementById('counter');
+        const incrementBtn = document.getElementById('incrementBtn');
+
+        let counterValue = 0;
+
+        incrementBtn.addEventListener('click', function() {
+            alert(`Current value: ${counterValue}`);
+            counterValue++;
+            counterElement.textContent = counterValue;
+        });
