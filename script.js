2beaadead7d324796997d762facd4279c140966e
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-27 14:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const counterElement = document.getElementById('counter');
-        const incrementBtn = document.getElementById('incrementBtn');
+//your JS code here. If required.
+const counterText=document.getElementById('counter');
+const counterBtn=document.getElementById('incrementBtn');
+	alert("Current Value: "+0);
 
-        let counterValue = 0;
 
-        incrementBtn.addEventListener('click', function() {
-            alert(`Current value: ${counterValue}`);
-            counterValue++;
-            counterElement.textContent = counterValue;
-        });
+let counterValue=0;
+counterBtn.addEventListener('click',function(){
+	counterValue++;
+	alert(`Current Value: ${counterValue}`);
+	counterText.textContent=counterValue;
+});
